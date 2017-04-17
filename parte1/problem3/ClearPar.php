dc97574046c5e43c1cf5325f1215e6ee4869c5b8
Gildus <gildus@gmail.com>
2017-04-17 17:30:51 -0500

    problema 3

--- parte1/problem3/ClearPar.php
+++ parte1/problem3/ClearPar.php
@@ -3,9 +3,25 @@ namespace Pigmalion\Parte1\Problem3;
 
 class ClearPar
 {
-    public function build()
+    public function build($cadenaDeSoloParentesis) : string
     {
+        $result = '';
+        if (strlen($cadenaDeSoloParentesis) <= 1) {
+            return '';
+        }
 
+        for ($i = 0; $i < strlen($cadenaDeSoloParentesis); ++$i) {
+            if (isset($cadenaDeSoloParentesis[$i+1])) {
+                if (ord($cadenaDeSoloParentesis[$i]) === 40 && ord($cadenaDeSoloParentesis[$i+1]) === 41) {
+                    $result .= $cadenaDeSoloParentesis[$i] . $cadenaDeSoloParentesis[++$i];
+                }
+            }
+        }
+
+        return $result;
     }
 
 }
+
+$input = trim(fgets(STDIN));
+echo (new ClearPar())->build($input);
