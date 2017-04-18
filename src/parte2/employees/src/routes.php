24eccb231904079e1579ff372484ec390d8b3437
Gildus <gildus@gmail.com>
2017-04-17 20:08:28 -0500

    vamos que sale

--- src/parte2/employees/src/routes.php
+++ src/parte2/employees/src/routes.php
@@ -1,24 +1,58 @@
 <?php
-// Routes
+
+// Register with container
+$container = $app->getContainer();
+$container['csrf'] = function ($c) {
+    return new \Slim\Csrf\Guard;
+};
+
+function searchEmployees($data, $need)
+{
+
+}
 
 $app->get('/', function ($request, $response, $args) {
-    // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
 })->setName('home');
 
 $app->get('/listado', function($request, $response) use($app) {
-
     $services = $app->getContainer();
     $data = $str = file_get_contents($services->get('data_employees'));
     $empleados = json_decode($data);
 
+    $nameKey = $this->csrf->getTokenNameKey();
+    $valueKey = $this->csrf->getTokenValueKey();
+    $name = $request->getAttribute($nameKey);
+    $value = $request->getAttribute($valueKey);
+
+    $tokenArray = [
+        $nameKey => $name,
+        $valueKey => $value
+    ];
+
+    return $this->renderer->render($response, 'listado.phtml', [
+        'empleados' => $empleados,
+        'csrfToken' => $tokenArray,
+
+    ]);
+})->add($container->get('csrf'))->setName('listado-empleados');
+
+$app->post('/listado', function($request, $response) use($app) {
+    $services = $app->getContainer();
+    $data = $str = file_get_contents($services->get('data_employees'));
+    $empleados = json_decode($data);
+
+    $email = $request->getParam('email');
+    $ok = $app->v::string()->notEmpty()->validate($email);
+    var_dump($ok); exit;
+    searchEmployees($email);
 
 
     return $this->renderer->render($response, 'listado.phtml', [
         'empleados' => $empleados
     ]);
+})->add($container->get('csrf'))->setName('listado-empleados');
 
-})->setName('listado-empleados');
 
 
 $app->get('/ver/empleado', function ($request, $response, $args) {
